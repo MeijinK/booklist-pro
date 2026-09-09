bbfada2fb143ebe8a181e39bd91494d970602d96
Emile Junior ADA <junada99@gmail.com>
2026-09-09 14:56:03 +0200

    feat: exposer le domaine via un baril

--- domain/index.ts
+++ domain/index.ts
@@ -1,2 +1,36 @@
-export type { Livre } from "./livre";
-export type { Note } from "./note";
+export {
+  BookDraftSchema,
+  BookSchema,
+  MIN_PUBLICATION_YEAR,
+  maxPublicationYear,
+  type Book,
+  type BookDraft,
+} from "./livre";
+
+export { NOTE_MAX_LENGTH, NoteSchema, type Note } from "./note";
+
+export { DEFAULT_LIMIT, MAX_LIMIT, pageSchema, type Page } from "./page";
+
+export {
+  ApiError,
+  ApiErrorBodySchema,
+  AUTH_CODES,
+  ConflictBodySchema,
+  type ApiErrorDetail,
+  type AuthCode,
+  type AuthError,
+  type ConflictError,
+  type NetworkError,
+  type ValidationError,
+} from "./erreurs";
+
+export {
+  DEFAULT_FILTERS,
+  READ_STATUSES,
+  SORT_FIELDS,
+  SORT_ORDERS,
+  type BookFilters,
+  type ReadStatus,
+  type SortField,
+  type SortOrder,
+} from "./filtres";
