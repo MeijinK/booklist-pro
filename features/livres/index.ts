56a2faf160f789a0b99fdeef4c32813a86399182
Emile Junior ADA <junada99@gmail.com>
2026-09-09 15:39:08 +0200

    feat: exposer les hooks d'ecriture des ouvrages

---
+++ features/livres/index.ts
@@ -0,0 +1,5 @@
+export { useBook } from "./useBook";
+export { useBooks } from "./useBooks";
+export { useCreateBook } from "./useCreateBook";
+export { useDeleteBook } from "./useDeleteBook";
+export { useUpdateBook, type UpdateBookInput } from "./useUpdateBook";
