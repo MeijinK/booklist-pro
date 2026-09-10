37cc8b18e56721340f1f87183b87d1d979bc6346
Emile Junior ADA <junada99@gmail.com>
2026-09-10 15:19:44 +0200

    feat: diffuser la palette par un contexte

--- theme/index.ts
+++ theme/index.ts
@@ -1,3 +1,10 @@
-export { colors } from "./colors";
+export {
+  darkColors,
+  lightColors,
+  palettes,
+  type ColorScheme,
+  type Palette,
+} from "./colors";
 export { MAX_TEXT_WIDTH, radius, space } from "./spacing";
 export { FONT_FAMILY, paperTheme } from "./paper";
+export { ThemeProvider, useAppTheme, useThemedStyles, type AppTheme } from "./ThemeProvider";
